09cb8648f6e79b5b0a29cbb7830d6ec193254ce8
Egoorbis <33342912+Egoorbis@users.noreply.github.com>
2026-04-01 20:54:20 +0200

    Refactor data sources in data.tf

--- infra/data.tf
+++ infra/data.tf
@@ -7,18 +7,3 @@ data "azurerm_container_registry" "existing" {
   resource_group_name = "rg-base-container"
 }
 
-# Data source for backend container app to get managed identity
-data "azurerm_container_app" "backend" {
-  name                = module.backend_container_app.resource.name
-  resource_group_name = azurerm_resource_group.main.name
-
-  depends_on = [module.backend_container_app]
-}
-
-# Data source for frontend container app to get managed identity
-data "azurerm_container_app" "frontend" {
-  name                = module.frontend_container_app.resource.name
-  resource_group_name = azurerm_resource_group.main.name
-
-  depends_on = [module.frontend_container_app]
-}
